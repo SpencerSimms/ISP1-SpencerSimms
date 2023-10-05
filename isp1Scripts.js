0416fc3b666270ae8b81b3137403a1c3fb158f7a
Spencer Simms <spencersimms0@gmail.com>
2023-10-04 20:23:26 -0400

    Final website with comments

--- isp1Scripts.js
+++ isp1Scripts.js
@@ -1,3 +1,4 @@
+// function that retrieves the user's browser info and displays it on the page
 function getBrowser() {
     let userAgent = navigator.userAgent;
     let browserName
@@ -16,18 +17,19 @@ function getBrowser() {
     document.getElementById("browser").innerHTML = "Browser: " + browserName;
 
 }
-
+// When the page loads, the function get browser is called
 document.addEventListener("DOMContentLoaded", getBrowser);
 
+// Used to add 0 before hour minute or second on the clock if they are single digits
 function addZero(num, digit) {
     return String(num).padStart(digit, '0')
 }
 
+// gets the time and displays it to the page
 function updateTime() {
     let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let now = new Date();
-
-    
+ 
     if (now.getHours() < 12) {
         document.getElementById("time").innerText = 
             addZero(now.getHours(), 2) + ":" +
@@ -47,12 +49,17 @@ function updateTime() {
         addZero(now.getFullYear(), 2);
 }
 
+// factorial calculator
 function factorialCalc() {
+    // user enters the number they want to find factorial of
     let num = prompt("Enter a positive number.", 8);
+    // if the user enters anything other than a number they are alerted
     if (isNaN(num)) {
         alert("Invalid input, enter a positive number.");
+    // if the number is negative the user is alerted that it is not allowed
     } else if (num < 0) {
         alert("Negative numbers not allowed.")
+    // if input is valid
     } else {
         let result = num
         if (result == 0) {
@@ -62,10 +69,12 @@ function factorialCalc() {
                 result *= (i - 1);
             }
         }
+        // outputs the final answer
         document.getElementById("result").textContent = ("The factorial of " + num + " is " + result + ".");
     }
 }
 
+// moves the center of the picture to where the user clicks
 function movePic() {
     let clickX = event.clientX;
     let clickY = event.clientY;
@@ -73,8 +82,10 @@ function movePic() {
     document.getElementById("AnakinPic").style.top = clickY + "px";
 }
 
+// detects when user clicks and calls movePic function
 document.addEventListener("click", movePic);
 
+// toggles between dark mode and light mode
 function changeTheme() {
     document.body.classList.toggle("lightMode");
 }
